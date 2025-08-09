2e8c551f5ca84fe98bb15b2679c327d0a9d305bf
Felix <fei1887415157@gmail.com>
2025-08-09 16:08:23 -0700

    misc

--- src/deploy.py
+++ src/deploy.py
@@ -1,4 +1,4 @@
 """
 Since developing in Windows and deploying on Jetson (Linux ARM), Can not deploy with PyInstaller.
-Use ONNX runtime with GPU instead.
+Use TensorRT .engine model format instead.
 """
